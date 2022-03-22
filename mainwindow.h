6663c9f42f63a47f723e1ef3b9dcc7bff768aca3
apelcinco <lazutin.leh@gmail.com>
2022-03-22 16:41:51 +0300

    ver 2.0.0

--- mainwindow.h
+++ mainwindow.h
@@ -14,59 +14,51 @@ QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	MainWindow(QWidget *parent = nullptr);
+	~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
+	void on_pushButton_clicked();
 
-    void on_lineEdit_14_textEdited(const QString &arg1);
+		void on_saveBt_clicked();
 
-    void on_lineEdit_15_textEdited(const QString &arg1);
+	void on_lineEdit_3_textChanged();
+	void nultext4();
+	void nultext8();
+	void nultext13();
+	void nultext18();
+	void on_lineEdit_2_textChanged();
 
-    void on_lineEdit_16_textEdited(const QString &arg1);
+	void on_lineEdit_textChanged();
 
-    void on_lineEdit_17_textEdited(const QString &arg1);
+	void on_lineEdit_5_textChanged();
 
-	void on_saveBt_clicked();
+	void on_lineEdit_6_textChanged();
 
-    void on_lineEdit_3_textChanged(const QString &arg1);
-    void nultext4();
-    void nultext8();
-    void nultext13();
-    void nultext18();
-    void on_lineEdit_2_textChanged(const QString &arg1);
+	void on_lineEdit_7_textChanged();
 
-    void on_lineEdit_textChanged(const QString &arg1);
+	void on_lineEdit_9_textChanged();
 
-    void on_lineEdit_5_textChanged(const QString &arg1);
+	void on_lineEdit_10_textChanged();
 
-    void on_lineEdit_6_textChanged(const QString &arg1);
+	void on_lineEdit_11_textChanged();
 
-    void on_lineEdit_7_textChanged(const QString &arg1);
+	void on_lineEdit_12_textChanged();
 
-    void on_lineEdit_9_textChanged(const QString &arg1);
+	void on_lineEdit_14_textChanged();
 
-    void on_lineEdit_10_textChanged(const QString &arg1);
+	void on_lineEdit_15_textChanged();
 
-    void on_lineEdit_11_textChanged(const QString &arg1);
+	void on_lineEdit_16_textChanged();
 
-    void on_lineEdit_12_textChanged(const QString &arg1);
-
-    void on_lineEdit_14_textChanged(const QString &arg1);
-
-    void on_lineEdit_15_textChanged(const QString &arg1);
-
-    void on_lineEdit_16_textChanged(const QString &arg1);
-
-    void on_lineEdit_17_textChanged(const QString &arg1);
+	void on_lineEdit_17_textChanged();
 
 private:
-    Ui::MainWindow *ui;
-    QSqlTableModel *model;
+	Ui::MainWindow *ui;
+	QSqlTableModel *model;
 
 };
 #endif // MAINWINDOW_H
